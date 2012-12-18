31a5a5291fb816abf15463d0def71a9c8713c18e
Govani <govaniso@hotmail.com>
2012-12-18 15:18:24 -0800

    KunbiaPHP con el framework de Gumby para el front-end

---
+++ kumbia+gumby/app/controllers/index_controller.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+/**
+ * Controller por defecto si no se usa el routes
+ * 
+ */
+class IndexController extends AppController
+{
+
+    public function index()
+    {
+        Flash::success("<a href='http://gumbyframework.com' target='_blank'>Más información sobre Gumby</a>");
+    }
+
+}
